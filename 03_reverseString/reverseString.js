37bd2eda8f37762a56b66e00a7546d8d58fff4ba
TJ Coetzee <theunis.coetzee19@gmail.com>
2024-06-11 16:59:46 -0500

    Finished 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    ans = '';
+    for (let i = s.length-1; i >= 0; i--) {
+        ans += s[i]
+    }
+    
+    return ans;
 };
 
 // Do not edit below this line

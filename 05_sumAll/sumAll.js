bd30cca3c6eb07ce8c90a885940f7f4c8e5a3b20
TJ Coetzee <theunis.coetzee19@gmail.com>
2024-06-11 17:20:00 -0500

    Finished 05, 06, and 07.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    } else if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
 
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let temp = 5/9 * (fahr - 32);
+  return parseFloat(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let temp = 9/5 * cel + 32;
+  return parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line

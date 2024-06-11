04ec556d2d86643f1f31be2c54783e6d6dedcd95
TJ Coetzee <theunis.coetzee19@gmail.com>
2024-06-11 16:42:24 -0500

    Finished 01 and 02 activites

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    if (num < 0) { return 'ERROR'; }
+    
+    let ans = '';
+    for (let i = 0; i < num; i++) {
+        ans += s;
+    }
+    return ans;
 };
 
 // Do not edit below this line

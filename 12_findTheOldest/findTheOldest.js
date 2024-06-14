cd6de6e8fcf793b6dbd0fc4cd93a145ac5ffaffe
TJ Coetzee <theunis.coetzee19@gmail.com>
2024-06-14 11:41:22 -0500

    Finished 08-12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        if (!curr['yearOfDeath']) { 
+            currAge = 2024 - curr['yearOfBirth']; 
+        } else {
+            currAge = curr['yearOfDeath'] - curr['yearOfBirth'];
+        }
 
+        if (!oldest['yearOfDeath']) {
+            oldestAge = 2024 - oldest['yearOfBirth'];
+        } else {
+            oldestAge = oldest['yearOfDeath'] - oldest['yearOfBirth'];
+        }
+
+        if (currAge > oldestAge) {
+            return curr;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
